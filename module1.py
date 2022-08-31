ce35aa19f246f372b0408a02044d0ad8e5f2987f
JoseSolizMejia <joseluissolizmejia48@gmial.com>
2022-08-31 12:26:53 -0300

    tercera actividad

--- module1.py
+++ module1.py
@@ -13,12 +13,18 @@ def main():
     a = 10
     b = 15
     print(a+b)
+    #este codigp suma las variables
 
     a = "¿Como "
     b = "estas?"
     print(a+b)
+    #este codigo contatena las variables
 
-
+    nombre = input("Ingrese su nombre")
+    dni = input("Ingrese su dni")
+    print(f'El nombre del usuario es {nombre}')
+    print(f'La dni es {dni}')
+    #este codigo pide al usuario que ingrese sus datos que le solicitamos
 
 if __name__ == '__main__':
     main()
