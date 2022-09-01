70992b7ba1080ea0bb87f0e52abb65699d2fcd00
JoseSolizMejia <joseluissolizmejia48@gmial.com>
2022-09-01 13:59:48 -0300

    subo ejercicio 1 del tp2

---
+++ TP2.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      josel
+#
+# Created:     01/09/2022
+# Copyright:   (c) josel 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    numero = int(input("Ingrese número impar: "))
+
+    if(numero%2!=0):
+        print("El numero es impar")
+
+if __name__ == '__main__':
+    main()
