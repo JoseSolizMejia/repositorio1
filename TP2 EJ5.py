0507c266e0357fa81d840125ee9884eb7434a6ca
JoseSolizMejia <joseluissolizmejia48@gmial.com>
2022-09-02 08:17:30 -0300

    subo ejercicio 5 del tp2

---
+++ TP2 EJ5.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      josel
+#
+# Created:     01/09/2022
+# Copyright:   (c) josel 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    lista = [1,2,3,4,5,6,7]
+    contador=0
+    for x in lista:
+        contador = contador + x
+    print(contador)
+
+if __name__ == '__main__':
+    main()
